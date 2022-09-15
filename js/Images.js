29c6586134571b090fa03c390a8c5f43e96cf227
Hakuryuu <dinhhuunghi96@gmail.com>
2022-09-15 12:51:09 +0900

    Add files via upload

---
+++ js/Images.js
@@ -0,0 +1,5 @@
+﻿var targetImg = document.getElementById('Image');
+var orgWidth  = targetImg.width;  // 元の横幅を保存
+var orgHeight = targetImg.height; // 元の高さを保存
+targetImg.width = 400;  // 横幅を400pxにリサイズ
+targetImg.height = orgHeight * (targetImg.width / orgWidth); // 高さを横幅の変化割合に合わせる
---
+++ js/magnific-popup-options.js
@@ -0,0 +1,1 @@
+$(document).ready(function(){var b=function(){$(".image-popup").magnificPopup({type:"image",removalDelay:300,mainClass:"mfp-with-zoom",gallery:{enabled:true}})};var a=function(){$(".popup-youtube, .popup-vimeo, .popup-gmaps").magnificPopup({disableOn:700,type:"iframe",mainClass:"mfp-fade",removalDelay:160,preloader:false,fixedContentPos:false})};b();a()});
